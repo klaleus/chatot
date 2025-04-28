06a6347ea1a387e788b307158bbbe9b9b8de06e9
Klaleus <klaleus@gmail.com>
2025-04-28 13:16:44 -0500

    added comments

--- include/chatot/chatot.h
+++ include/chatot/chatot.h
@@ -18,7 +18,14 @@ ChatotSeverity;
 // Functions
 ////////////////////////////////////////////////////////////////////////////////
 
+// Sets the minimum severity requirement for messages passed to `chatot_chatter()`.
+// The default severity is `CHATOT_SEVERITY_DEBUG`.
 void chatot_severity(ChatotSeverity severity);
+
+// Prints formatted messages to `stdout`.
+// Messages are prefixed with their corresponding severity.
+// If a message does not meet the minimum severity requirement, then it will not be printed.
+// If a message has `CHATOT_SEVERITY_FATAL`, then `exit(EXIT_FAILURE)` will be called.
 void chatot_chatter(ChatotSeverity severity, const char* message, ...);
 
 #endif // CHATOT_GUARD
